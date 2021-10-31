1aa71e936ab23ca8b055f226c4b1aed13dacd8de
Elizabeth Van Elsen <54674146+Lizzie2222000@users.noreply.github.com>
2021-10-31 14:58:37 -0400

    Added a bellow image to the bellow minigame

--- Fantasy Forge/Assets/Scripts/FurnaceScript.cs
+++ Fantasy Forge/Assets/Scripts/FurnaceScript.cs
@@ -11,11 +11,22 @@ public class FurnaceScript : MonoBehaviour
     public GameObject complete;
     private float timer;
     public float time;
+    //public GameObject bellowObject;
+    public Sprite bellow1;
+    public Sprite bellow2;
+    public Sprite bellow3;
+    public Sprite bellow4;
+    public Sprite bellow5;
+    public Image bellowImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        heat.value = 0;
+        bellow.value = (float)22.5;
+
+        //bellowImage = bellowObject.GetComponent<Image>();
+        bellowImage.sprite = bellow1;
     }
 
     // Update is called once per frame
@@ -40,9 +51,29 @@ public class FurnaceScript : MonoBehaviour
 
     public void pump()
     {
-        if(bellow.value == 1)
+        if(bellow.value == 0)
         {
             heat.value++;
+        }
+        if(bellow.value > 16.9)
+        {
+            bellowImage.sprite = bellow1;
+        }
+        else if(bellow.value > 13.3)
+        {
+            bellowImage.sprite = bellow2;
+        }
+        else if(bellow.value > 9.0)
+        {
+            bellowImage.sprite = bellow3;
+        }
+        else if(bellow.value > 1.8)
+        {
+            bellowImage.sprite = bellow4;
+        }
+        else
+        {
+            bellowImage.sprite = bellow5;
         }
     }
 }
