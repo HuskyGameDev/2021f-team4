ee4f39068a1c7cd0bc1698162537b1be67bb7183
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2022-02-03 19:48:42 -0500

    Revert "Merge branch 'main' of https://github.com/HuskyGameDev/2021f-team4"
    This reverts commit 35b975272bd7b21353a85d78d1be2412c7b073d7, reversing
    changes made to 67083228aae8ccd7166155686c195aebfb2d0c6e.

--- Fantasy Forge/Assets/Scripts/DeployCustomers.cs
+++ Fantasy Forge/Assets/Scripts/DeployCustomers.cs
@@ -20,6 +20,7 @@ public class DeployCustomers : MonoBehaviour
     {
         GameObject customer = Instantiate(customerPrefab) as GameObject;
         customer.transform.position = new Vector2((float) -3.4, 6);
+        //cust.transform.position.Set(1, 1, 1);
     }
 
     //Timing for when customers spawn.
