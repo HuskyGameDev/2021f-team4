823972712dbd505bf54e5e7344c6fd85194b18f9
MarieSZgurich <mszguric@mtu.edu>
2022-03-23 15:27:54 -0400

    Pause Menu WIP
    Press Escape. Buttons not currently functional

---
+++ Fantasy Forge/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
